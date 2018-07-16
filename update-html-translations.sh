29372c68c4ccf70dc1c84b491d172a64c9be4576
Mario Izquierdo <hola@marioizquierdo.es>
2018-07-16 14:01:55 +0200

    Update pot and spanish po

--- update-html-translations.sh
+++ update-html-translations.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# workingdir=$(pwd)
-# for pot in $(ls $workingdir/data/po/)
-# do
-#   rm $workingdir/data/po/$pot/$pot.pot -v
-# done
-# rm -f po/max-mate-welcome.pot
-# ./welcome-po.py --create-pot
-# ./edgar-allan create-all-pots
+workingdir=$(pwd)
+for pot in $(ls $workingdir/data/po/)
+do
+  rm $workingdir/data/po/$pot/$pot.pot -v
+done
+rm -f po/max-mate-welcome.pot
+./welcome-po.py --create-pot
+./edgar-allan create-all-pots
 
 for html in $(find data/po/ -mindepth 1 -type d); do
 
@@ -15,9 +15,10 @@ for html in $(find data/po/ -mindepth 1 -type d); do
     for p in $(find ${html} -name "*.po"); do
         echo $p in $html
         echo "  msgmerge -v ${p} ${pot} -U"
+        msgmerge -v ${p} ${pot} -U
     done
-
 done
+find data/po/ -name "*.po~" -delete
 
 
 # rm -rf ./locale/ ./data/i18n/
