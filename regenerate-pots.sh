386ef5582dd0091e3ee13fd9404e6772ec8e1ec1
Mario Izquierdo <hola@marioizquierdo.es>
2018-10-24 08:56:12 +0200

    Update gettingstarted (live mode)

--- regenerate-pots.sh
+++ regenerate-pots.sh
@@ -13,3 +13,13 @@ rm po/ubuntu-mate-welcome.pot
 ./welcome-po.py --create-pot
 ./edgar-allan create-all-pots
 
+find data/ -name "*.old" -delete
+
+# MAX merge po with translations (update po files)
+for d in $(find data/po/ -mindepth 1 -maxdepth 1 -type d); do
+    POT=$(find ${d} -type f -name "*.pot")
+    for p in $(find ${d} -type f -name "*.po"); do
+        echo msgmerge -v ${p} ${POT} -U
+        msgmerge -v ${p} ${POT} -U
+    done
+done
